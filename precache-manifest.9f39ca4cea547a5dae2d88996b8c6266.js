b35b3a855b5af6e2cba55ebd8a96d9389137050f
igaryok <igor.sobko@gmail.com>
2019-09-29 22:13:17 +0300

    Updates

---
+++ precache-manifest.9f39ca4cea547a5dae2d88996b8c6266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b4801ef2f79c5b2a1d77bf44d46153",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "604131bf84f348c9394a",
+    "url": "/react_tabs-with-router/static/css/main.7d243aa0.chunk.css"
+  },
+  {
+    "revision": "5aed6397df8223672528",
+    "url": "/react_tabs-with-router/static/js/2.10d81030.chunk.js"
+  },
+  {
+    "revision": "604131bf84f348c9394a",
+    "url": "/react_tabs-with-router/static/js/main.c9dcd631.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
